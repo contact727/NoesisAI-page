4bf02b5c505a5dd544be6694c7e1841c3e3c5eea
contact727 <contact@noesisiai.pro>
2026-01-20 13:44:31 +0100

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,8 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  // CORRECTION ICI : on a enlevé "client/" devant les chemins
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
